e5ad60456261fdd6a664458f71c41dae9c09a22b
loveforgeter <loveforgeter@gmail.com>
2016-10-24 17:21:19 +0800

    fix title bug

--- HMSegmentedControl/HMSegmentedControl.m
+++ HMSegmentedControl/HMSegmentedControl.m
@@ -331,7 +331,9 @@
       CATextLayer *titleLayer = [CATextLayer layer];
       titleLayer.frame = rect;
       titleLayer.alignmentMode = kCAAlignmentCenter;
-      titleLayer.truncationMode = kCATruncationEnd;
+      if ([UIDevice currentDevice].systemVersion.floatValue < 10) {
+        titleLayer.truncationMode = kCATruncationEnd;
+      }
       titleLayer.string = [self attributedTitleAtIndex:idx];
       titleLayer.contentsScale = [[UIScreen mainScreen] scale];
       
@@ -468,7 +470,9 @@
       titleLayer.frame = textRect;
       titleLayer.alignmentMode = kCAAlignmentCenter;
       titleLayer.string = [self attributedTitleAtIndex:idx];
-      titleLayer.truncationMode = kCATruncationEnd;
+      if ([UIDevice currentDevice].systemVersion.floatValue < 10) {
+        titleLayer.truncationMode = kCATruncationEnd;
+      }
       
       CALayer *imageLayer = [CALayer layer];
       imageLayer.frame = imageRect;
@@ -774,8 +778,11 @@
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
   UITouch *touch = [touches anyObject];
   CGPoint touchLocation = [touch locationInView:self];
-  
-  if (CGRectContainsPoint(self.bounds, touchLocation)) {
+  CGRect enlargeRect =   CGRectMake(self.bounds.origin.x - self.enlargeEdgeInset.left,
+                                    self.bounds.origin.y - self.enlargeEdgeInset.top,
+                                    self.bounds.size.width + self.enlargeEdgeInset.left + self.enlargeEdgeInset.right,
+                                    self.bounds.size.height + self.enlargeEdgeInset.top + self.enlargeEdgeInset.bottom);
+  if (CGRectContainsPoint(enlargeRect, touchLocation)) {
     NSInteger segment = 0;
     if (self.segmentWidthStyle == HMSegmentedControlSegmentWidthStyleFixed) {
       segment = (touchLocation.x + self.scrollView.contentOffset.x) / self.segmentWidth;
